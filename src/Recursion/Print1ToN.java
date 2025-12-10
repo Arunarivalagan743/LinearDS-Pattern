91b8ed84fb7a61bff0c68ebe87f555da8e6091cf
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-12-10 09:28:03 +0530

    first : sorting and recurion

--- src/Recursion/Print1ToN.java
+++ src/Recursion/Print1ToN.java
@@ -1,4 +1,18 @@
 package Recursion;
 
 public class Print1ToN {
-}
+
+        public static void main(String[] args) {
+            print(4,4);
+        }
+        public static void print(int i,int n) {
+            if(i < 1)
+            {
+                return;
+            }
+
+            print(i - 1,n );
+            System.out.println(i);
+        }
+    }
+
--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,4 +1,41 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        BubbleSort(arr,n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+    public static void BubbleSort(int[] a, int n) {
+        for(int i  =0;i<n;i++)
+        {
+            boolean swap = false;
+            for(int j  =0;j<n-1-i;j++)
+            {
+
+                if(a[j] > a[j+1])
+                {
+                        int temp = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = temp;
+                        swap = true;
+                }
+            }
+            if(swap == false)
+            {
+                break;
+
+            }
+        }
+    }
 }
