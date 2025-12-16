97c6e05bc9beec20a4e47da91d0c7c7d4f09100a
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-12-16 15:02:02 +0530

    quick sort by mid pivot

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -4,39 +4,40 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int a[] ={99,7,6,5,4,3,2,1,3,23,1};
-        quick(a,0,a.length);
-        System.out.println(Arrays.toString(a));
-    }
-    public static void quick(int a[],int l,int h)
-    {
-        if(l<h)
-        {
-            int pivot =par(a,l,h - 1); //taking partion to pick pivot
-            quick(a,0,pivot -1); //using pivoit to divide the oartition recurivly as two left and right by two methods
-            quick(a,pivot+1,h);
-        }
-    }
-    public static int par(int a[],int l,int h)
-    {
-        int pivot = a[h]; //intially taking a last elemnt as pivot to compare the elment to low to high -1;
-        int i  = l -1; //why? means to swap as first elamnt so start wiyj low -   1 means it be suppose to swap with next elamt which less than pivot
+        int[] arr = {10,7,8,9,1,5};
+        sort(arr, 0, arr.length - 1);
+      System.out.println(Arrays.toString(arr));
 
-        for(int j  =  l;j<=h -1;j++) //startiing with low to high -1 .why high 1 means avoud the pivot elambt swap
+    }
+
+    static void sort(int[] nums, int low, int hi) {
+        if(low >= hi) {
+            return;
+        }
+        int s = low;
+        int e =  hi;
+        int m = low+(hi-low)/2;
+        int pivot =  nums[m];
+        while(s<=e)
         {
-            if(a[j]<pivot) //making the arry use of pivot to make left side as less than pivot and finllay automaticlly it arrage in the right as greater tahn pivot
+            while(nums[s]<pivot)
             {
-                i++; //if less tahn swap;
-                swap(a,i,j);
+                s++;
+            }
+            while(nums[e]>pivot)
+            {
+                e--;
+            }
+            if(s<=e)
+            {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
             }
         }
-        swap(a,i+1,h); //for returing the pivot elamnt index so swap with h is pivot elemnst swap with where be low be ends ,out the next index as pivot elemnt to tell the partion to return to the partion by low + 1 as i +  1
-        return i+1; //return the pivot index
-    }
-    public static  void swap(int a[],int i,int j)
-    {
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
+        sort(nums, low, e);
+        sort(nums, s, hi);
     }
 }
